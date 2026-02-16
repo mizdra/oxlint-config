51407005ec5530b223f579877e28d3329c7807f6
mizdra <pp.mizdra@gmail.com>
2026-02-16 23:59:58 +0900

    export configs from packages

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import base from "./base.json" with { type: "json" };
+import typescript from "./typescript.json" with { type: "json" };
+import node from "./node.json" with { type: "json" };
+import react from "./react.json" with { type: "json" };
+
+const mizdra = {
+  base,
+  typescript,
+  node,
+  react,
+};
+
+export default mizdra;
